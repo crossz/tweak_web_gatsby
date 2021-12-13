cca0dee54474fbb551d32f48e4c628280c370c36
ajay <ajay.wu@take2.health>
2021-12-13 15:39:38 +0800

    :lipstick: Update t&c style

--- src/templates/T&C.js
+++ src/templates/T&C.js
@@ -1,93 +1,29 @@
 import React from 'react'
 import MdxLayout from '@layouts/MdxLayout'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 
-import {
-  makeStyles,
-  alpha,
-  Typography,
-  Container,
-  Box,
-  // Breadcrumbs,
-  // Hidden,
-} from '@material-ui/core'
-// import ArrowIcon from '@images/icons/arrow.svg'
-import useMenu from '@hooks/useMenu'
-import Links from '@components/WhatsNew/Links'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
-import { StaticImage } from 'gatsby-plugin-image'
-import { POST_TYPES } from '@utils/constant'
+import { makeStyles, Typography, Container, Box } from '@material-ui/core'
+
+// import { StaticImage } from 'gatsby-plugin-image'
 
 const useStyles = makeStyles((theme) => ({
-  root: {},
-  breadcrumbsWrapper: {
-    position: 'relative',
-    zIndex: 1,
-    height: 150,
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText,
-    display: 'flex',
-    alignItems: 'center',
-    fontSize: theme.typography.body2.fontSize,
-    '& a': {
-      color: theme.palette.primary.contrastText,
-    },
-    [theme.breakpoints.down('xs')]: {
-      height: 85,
-    },
-  },
-  breadcrumbsTitle: {
-    color: theme.palette.primary.contrastText,
-    maxWidth: theme.spacing(40),
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
-  },
-  arrowIcon: {
-    width: theme.spacing(1.5),
-    height: theme.spacing(1.5),
-    '& path': {
-      fill: theme.palette.primary.contrastText,
-    },
+  root: {
+    backgroundColor: theme.palette.background.paper,
   },
   header: {
     paddingTop: theme.spacing(8),
     marginBottom: theme.spacing(3),
   },
-  top: {
-    display: 'flex',
-    alignItems: 'center',
-    marginBottom: theme.spacing(3),
-    [theme.breakpoints.down('xs')]: {
-      alignItems: 'flex-start',
-    },
-  },
-  topLeft: {
-    display: 'flex',
-    alignItems: 'center',
-    [theme.breakpoints.down('xs')]: {
-      display: 'block',
-    },
-  },
   date: {
     color: theme.palette.text.primary,
     fontSize: theme.typography.body1.fontSize,
-    fontWeight: theme.typography.fontWeightBold,
     marginRight: theme.spacing(3),
     [theme.breakpoints.down('xs')]: {
       marginTop: theme.spacing(1.5),
       marginBottom: theme.spacing(1),
       marginRight: theme.spacing(2),
       fontSize: theme.typography.body2.fontSize,
-      fontWeight: theme.typography.fontWeightMedium,
     },
-  },
-  mark: {
-    fontSize: theme.typography.overline.fontSize,
-    color: theme.palette.secondary.contrastText,
-    // backgroundColor: theme.palette.secondary.main,
-    padding: theme.spacing(0.25, 1),
-    display: 'inline-block',
   },
   contentWrapper: {
     position: 'relative',
@@ -98,15 +34,6 @@ const useStyles = makeStyles((theme) => ({
     position: 'relative',
     zIndex: 2,
   },
-  image: {
-    marginTop: theme.spacing(-4),
-    marginBottom: theme.spacing(-5),
-    borderRadius: theme.spacing(1.25),
-    overflow: 'hidden',
-    [theme.breakpoints.down('xs')]: {
-      marginTop: theme.spacing(-6),
-    },
-  },
   postBg: {
     width: '100%',
     height: 980,
@@ -115,13 +42,6 @@ const useStyles = makeStyles((theme) => ({
     left: 0,
     [theme.breakpoints.down('xs')]: {
       height: 400,
-    },
-  },
-  moreWrapper: {
-    padding: theme.spacing(5, 3),
-    backgroundColor: alpha(theme.palette.primary.main, 0.03),
-    [theme.breakpoints.down('xs')]: {
-      backgroundColor: '#F8F9FA',
     },
   },
 }))
@@ -137,22 +57,18 @@ const Post = ({ data }) => {
         <Box className={classes.contentWrapper}>
           <Container className={classes.content} disableGutters maxWidth='sm'>
             <Box className={classes.header}>
-              <Box className={classes.top}>
-                <Box className={classes.topLeft}>
-                  <Box className={classes.date}>{date}</Box>
-                </Box>
-              </Box>
               <Typography variant='h5' color='primary'>
                 {title}
               </Typography>
+              <Box className={classes.date}>{date}</Box>
             </Box>
             <MdxLayout>{mdx}</MdxLayout>
           </Container>
-          <StaticImage
+          {/* <StaticImage
             className={classes.postBg}
             src='../assets/images/post_bg.png'
             alt='post background'
-          ></StaticImage>
+          ></StaticImage> */}
         </Box>
       </Container>
     </Box>
