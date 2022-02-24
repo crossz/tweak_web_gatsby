58400df3dab9568648fb0560a086834eb1ed7a2a
ajay <ajay.wu@take2.health>
2022-02-24 19:36:19 +0800

    :coffin: hidden zh-cn

--- languages.js
+++ languages.js
@@ -1,10 +1,18 @@
 module.exports = {
-  languages: ['zh-HK', 'zh-CN', 'en'],
-  languagePrefixes: { 'zh-HK': 'Hk', 'zh-CN': 'Cn', en: 'En' },
+  languages: [
+    'zh-HK',
+    // 'zh-CN',
+    'en',
+  ],
+  languagePrefixes: {
+    'zh-HK': 'Hk',
+    // 'zh-CN': 'Cn',
+    en: 'En',
+  },
   defaultLanguage: `zh-HK`,
   languageLabels: [
     { shortLabel: '繁', label: '繁體中文', lang: 'zh-HK' },
-    { shortLabel: '间', label: '简体中文', lang: 'zh-CN' },
+    // { shortLabel: '间', label: '简体中文', lang: 'zh-CN' },
     { shortLabel: 'EN', label: 'English', lang: 'en' },
   ],
 }
