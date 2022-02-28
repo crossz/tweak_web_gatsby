1b51214b9a863f625d760df3305ec37bdd82f8d2
ajay <ajay.wu@take2.health>
2022-02-28 16:52:58 +0800

    :lipstick: update mdx audio

--- src/layouts/Audio.js
+++ src/layouts/Audio.js
@@ -1,11 +1,51 @@
 import React from 'react'
-import { Box } from '@material-ui/core'
+import {
+  makeStyles,
+  Box,
+  useTheme,
+  useMediaQuery,
+  Typography,
+} from '@material-ui/core'
 import { MDX_MEDIA_MAXWIDTH } from '@utils/constant'
 
-const Audio = ({ src, ...rest }) => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: MDX_MEDIA_MAXWIDTH,
+    margin: '0 auto',
+  },
+  audio: {
+    width: '100%',
+  },
+}))
+
+const Audio = ({ src, title, describe, ...rest }) => {
+  const classes = useStyles()
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('xs'))
+
   return src ? (
-    <Box maxWidth={MDX_MEDIA_MAXWIDTH} mx='auto'>
-      <audio controls preload='auto' src={src} {...rest}></audio>
+    <Box className={classes.root}>
+      {title && (
+        <Box my={1.5}>
+          <Typography variant='h6' color='primary'>
+            {title}
+          </Typography>
+        </Box>
+      )}
+      <audio
+        className={classes.audio}
+        controls
+        preload='auto'
+        src={src}
+        {...rest}
+      ></audio>
+      {describe && (
+        <Box my={0.5} color='grey.600'>
+          <Typography variant={isMobile ? 'caption' : 'body2'}>
+            {describe}
+          </Typography>
+        </Box>
+      )}
     </Box>
   ) : null
 }
