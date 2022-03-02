de8a2bed0c5231919dfefcfb4b3fb89257211877
ajay <ajay.wu@take2.health>
2022-03-02 15:05:40 +0800

    :construction: add campaign page v2 component

--- src/pages/whats-new/campaign/index.js
+++ src/pages/whats-new/campaign/index.js
@@ -1,6 +1,21 @@
 import React from 'react'
-import Campaign from '@components/Campaign'
+import { graphql } from 'gatsby'
+import Campaign from '@components/CampaignV2'
 
 const CampaignRoot = () => <Campaign></Campaign>
 
 export default CampaignRoot
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`
