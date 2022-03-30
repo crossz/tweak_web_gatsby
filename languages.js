8af5e9e77a100d0e25023d3548b82f4a5f1540c4
ajay <ajay.wu@take2.health>
2022-03-30 11:15:42 +0800

    :speech_ballon: fix language short label

--- languages.js
+++ languages.js
@@ -8,7 +8,7 @@ module.exports = {
   defaultLanguage: `zh-HK`,
   languageLabels: [
     { shortLabel: '繁', label: '繁體中文', lang: 'zh-HK' },
-    { shortLabel: '间', label: '简体中文', lang: 'zh-CN' },
+    { shortLabel: '简', label: '简体中文', lang: 'zh-CN' },
     { shortLabel: 'EN', label: 'English', lang: 'en' },
   ],
 }
