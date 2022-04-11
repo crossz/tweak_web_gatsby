e78e1be38e4db0743393daada375293a8a99f681
tailong.wang@take2.health <tailong.wang@take2.health>
2022-04-11 12:33:23 +0800

    格式化代码

--- src/components/CampaignV2/Components/ContactReference.js
+++ src/components/CampaignV2/Components/ContactReference.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import {
   useTheme,
   useMediaQuery,
@@ -11,15 +11,20 @@ import {
   Grid,
   Button,
   IconButton,
-} from '@material-ui/core'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import { useI18next } from 'gatsby-plugin-react-i18next'
-import Link from '@components/Link'
-import MailIcon from '../images/mail.svg'
-import PhoneIcon from '../images/phone.svg'
-import WhatsAppIcon from '../images/WhatsApp.svg'
-import FacebookIcon from '../images/facebook.svg'
-import { WHATS_APP_LINK, SERVICE_EMAIL, SERVICE_PHONE, FACEBOOK_LINK } from '../utils/constant'
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { useI18next } from 'gatsby-plugin-react-i18next';
+import Link from '@components/Link';
+import MailIcon from '../images/mail.svg';
+import PhoneIcon from '../images/phone.svg';
+import WhatsAppIcon from '../images/WhatsApp.svg';
+import FacebookIcon from '../images/facebook.svg';
+import {
+  WHATS_APP_LINK,
+  SERVICE_EMAIL,
+  SERVICE_PHONE,
+  FACEBOOK_LINK,
+} from '../utils/constant';
 
 const useStyles = makeStyles((theme) => ({
   websiteButton: {
@@ -82,7 +87,7 @@ const useStyles = makeStyles((theme) => ({
       padding: theme.spacing(0, 0, 3, 0),
     },
   },
-}))
+}));
 
 const CONTACT_LIST = [
   {
@@ -100,16 +105,16 @@ const CONTACT_LIST = [
     href: `mailto:${SERVICE_EMAIL}`,
     icon: <MailIcon />,
   },
-]
+];
 
 const ContactReference = () => {
-  const { t } = useI18next()
-  const theme = useTheme()
-  const isMobile = useMediaQuery(theme.breakpoints.down('xs'))
+  const { t } = useI18next();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
   const classes = useStyles({
     progressRightWidth: isMobile ? 80 : 316,
     isMobile,
-  })
+  });
 
   const reference = [
     {
@@ -139,7 +144,7 @@ const ContactReference = () => {
     {
       list: '9. Overview of Hong Kong Cancer Statistics of 2019. Hong Kong Hospital Authority, October 2021.',
     },
-  ]
+  ];
   const rules = [
     {
       rulesDetail:
@@ -157,26 +162,39 @@ const ContactReference = () => {
       rulesDetail:
         'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
     },
-  ]
+  ];
   return (
     <Box
-      id="gsap-scroll-to-contact-us"
+      id='gsap-scroll-to-contact-us'
       pt={isMobile ? 5 : 8}
       pb={isMobile ? 0 : 13.5}
       px={2.5}
       bgcolor={isMobile ? 'transparent' : 'background.paper'}
     >
-      <Box className="gsap-fade-in-11-trigger gsap-fade-in-11" mx="auto" maxWidth={1192}>
-        <Box color="prophecyPrimary.main" fontWeight="fontWeightBold" fontSize={24}>
+      <Box
+        className='gsap-fade-in-11-trigger gsap-fade-in-11'
+        mx='auto'
+        maxWidth={1192}
+      >
+        <Box
+          color='prophecyPrimary.main'
+          fontWeight='fontWeightBold'
+          fontSize={24}
+        >
           {t('cp_v2.contact_and_reference.paragraphs.0')}
         </Box>
-        <Box py={isMobile ? 5 : 6} borderBottom="1px solid #E1E1E1">
-          <Box fontWeight="fontWeightMedium" fontSize="body1.fontSize" color="grey.900" mb={2}>
+        <Box py={isMobile ? 5 : 6} borderBottom='1px solid #E1E1E1'>
+          <Box
+            fontWeight='fontWeightMedium'
+            fontSize='body1.fontSize'
+            color='grey.900'
+            mb={2}
+          >
             {t('cp_v2.contact_and_reference.paragraphs.1')}
           </Box>
           <Box display={isMobile ? 'block' : 'flex'}>
             {CONTACT_LIST.map((item, index) => (
-              <Link key={index} underline="none" to={item.href} target="_blank">
+              <Link key={index} underline='none' to={item.href} target='_blank'>
                 <Box
                   bgcolor={!index ? '#25D366' : 'prophecyPrimary.light'}
                   borderRadius={12}
@@ -184,17 +202,17 @@ const ContactReference = () => {
                   mb={isMobile ? 2 : 0}
                   py={2}
                   px={3}
-                  display="flex"
-                  alignItems="center"
+                  display='flex'
+                  alignItems='center'
                 >
                   <Box mr={1} flexShrink={0}>
                     {item.icon}
                   </Box>
                   <Box
-                    fontSize="body1.fontSize"
-                    fontWeight="fontWeightBold"
-                    color="primary.contrastText"
-                    component="span"
+                    fontSize='body1.fontSize'
+                    fontWeight='fontWeightBold'
+                    color='primary.contrastText'
+                    component='span'
                   >
                     {item.title}
                   </Box>
@@ -205,16 +223,25 @@ const ContactReference = () => {
         </Box>
         <Grid container spacing={0}>
           <Grid item xs={12} sm={6}>
-            <Box height="100%" py={isMobile ? 5 : 6} borderBottom="1px solid #E1E1E1">
-              <Box fontWeight="fontWeightMedium" fontSize="body1.fontSize" color="grey.900" mb={2}>
+            <Box
+              height='100%'
+              py={isMobile ? 5 : 6}
+              borderBottom='1px solid #E1E1E1'
+            >
+              <Box
+                fontWeight='fontWeightMedium'
+                fontSize='body1.fontSize'
+                color='grey.900'
+                mb={2}
+              >
                 {t('cp_v2.contact_and_reference.paragraphs.2')}
               </Box>
-              <Link to="/">
+              <Link to='/'>
                 <Button
-                  size="small"
+                  size='small'
                   fullWidth={isMobile}
-                  variant="outlined"
-                  color="inherit"
+                  variant='outlined'
+                  color='inherit'
                   className={classes.websiteButton}
                 >
                   {t('cp_v2.menu.office_website')}
@@ -223,12 +250,26 @@ const ContactReference = () => {
             </Box>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <Box height="100%" py={isMobile ? 5 : 6} borderBottom="1px solid #E1E1E1">
-              <Box fontWeight="fontWeightMedium" fontSize="body1.fontSize" color="grey.900" mb={2}>
+            <Box
+              height='100%'
+              py={isMobile ? 5 : 6}
+              borderBottom='1px solid #E1E1E1'
+            >
+              <Box
+                fontWeight='fontWeightMedium'
+                fontSize='body1.fontSize'
+                color='grey.900'
+                mb={2}
+              >
                 {t('cp_v2.contact_and_reference.paragraphs.3')}
               </Box>
               <Box height={56}>
-                <IconButton href={FACEBOOK_LINK} target="_blank" size="small" aria-label="facebook">
+                <IconButton
+                  href={FACEBOOK_LINK}
+                  target='_blank'
+                  size='small'
+                  aria-label='facebook'
+                >
                   <FacebookIcon />
                 </IconButton>
               </Box>
@@ -245,8 +286,8 @@ const ContactReference = () => {
             >
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
+                aria-controls='panel1a-content'
+                id='panel1a-header'
                 classes={{
                   root: classes.summaryRoot,
                 }}
@@ -275,13 +316,15 @@ const ContactReference = () => {
             >
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
+                aria-controls='panel1a-content'
+                id='panel1a-header'
                 classes={{
                   root: classes.summaryRoot,
                 }}
               >
-                <Typography className={classes.heading}>{t('t_and_c.terms_and_conditions')}</Typography>
+                <Typography className={classes.heading}>
+                  {t('t_and_c.terms_and_conditions')}
+                </Typography>
               </AccordionSummary>
               <AccordionDetails
                 classes={{
@@ -322,7 +365,7 @@ const ContactReference = () => {
         )}
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default ContactReference
+export default ContactReference;
