8148fb775a1b12e917211774dbe269ebe92051a3
tailong.wang@take2.health <tailong.wang@take2.health>
2022-06-14 17:06:38 +0800

    fix

--- src/components/CampaignV2/Components/AthletePostCard.js
+++ src/components/CampaignV2/Components/AthletePostCard.js
@@ -93,7 +93,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-const PostCard = ({
+const AthletePostCard = ({
   title,
   detail,
   type,
@@ -165,4 +165,4 @@ const PostCard = ({
   )
 }
 
-export default PostCard
+export default AthletePostCard
